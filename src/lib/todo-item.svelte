cf3bf731b7399a4aec207986ea68cd773dd20915
Harsh Yadav <glorytoharsh@gmail.com>
2021-12-14 11:40:35 +0000

    Create todoitem and layout for the app

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,14 @@
+<div>
+    <form action="" method="">
+        <input type="hidden" name="done" value="" >
+        <button aria-label="Mark Done/Not Done">Done/Not Done</button>
+    </form>
+    <form action="" method="">
+        <input type="text" value="">
+        <button aria-label="Save Todo">Save</button> 
+    </form>
+    <form action="" method="">
+        <button aria-label="Delte Todo">Delete</button>
+    </form>
+
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,21 @@
+<script lang="ts">
+	// lib is has preconfigured path variable, can be accesed as follow:
+	import TodoItem from '$lib/todo-item.svelte';
+
+	const title = 'Todo';
+</script>
+
+<svelte:head><title>{title}</title></svelte:head>
+
+
 <section>
 	<h1>
-		Welcome
+		{title} <i>Svelte</i>
 	</h1>
+	<form action="" method="POST">
+		<input type="text" name="todo" placeholder="+ Type to add todo" aria-label="add todo" />
+	</form>
+
+	<TodoItem />
 </section>
+
