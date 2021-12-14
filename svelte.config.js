eb70c787396cf7537aefa7f57a27d4a4de0ba541
Harsh Yadav <glorytoharsh@gmail.com>
2021-12-14 09:32:02 +0000

    refactor: remove boilerplate

--- svelte.config.js
+++ svelte.config.js
@@ -5,10 +5,10 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess(), // sass, etc
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter(), // deployment option such as vercel .
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
